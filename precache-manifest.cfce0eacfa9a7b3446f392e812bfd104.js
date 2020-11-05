fb61279b51625db4401ef00512b7bd8e17b6d967
mirkoculibrk <micoculibrk@gmail.com>
2020-11-05 12:56:38 +0100

    Updates

---
+++ precache-manifest.cfce0eacfa9a7b3446f392e812bfd104.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69497029c8963c3a1fc07c63c319c8b",
+    "url": "/ReactJS-RecipeApp/index.html"
+  },
+  {
+    "revision": "87f864218f9d7d5a1c94",
+    "url": "/ReactJS-RecipeApp/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1d285e1857900bcbeb5f",
+    "url": "/ReactJS-RecipeApp/static/css/main.925be8dc.chunk.css"
+  },
+  {
+    "revision": "87f864218f9d7d5a1c94",
+    "url": "/ReactJS-RecipeApp/static/js/2.329f3399.chunk.js"
+  },
+  {
+    "revision": "93faeebb3cccb9190fbc1347892ed539",
+    "url": "/ReactJS-RecipeApp/static/js/2.329f3399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d285e1857900bcbeb5f",
+    "url": "/ReactJS-RecipeApp/static/js/main.6884c2b6.chunk.js"
+  },
+  {
+    "revision": "a93518a286e2734f5622",
+    "url": "/ReactJS-RecipeApp/static/js/runtime-main.a574f90c.js"
+  },
+  {
+    "revision": "2f856b1389ae9ad171c6d363e02844ca",
+    "url": "/ReactJS-RecipeApp/static/media/close.2f856b13.svg"
+  },
+  {
+    "revision": "e5bb08d778c246cc52f2276e560fcd28",
+    "url": "/ReactJS-RecipeApp/static/media/expandicon.e5bb08d7.svg"
+  },
+  {
+    "revision": "dfcfef492bf439dc02eaaa26212fbdee",
+    "url": "/ReactJS-RecipeApp/static/media/hearthempty.dfcfef49.svg"
+  },
+  {
+    "revision": "53af0b3e74763cf509bb944de0eed800",
+    "url": "/ReactJS-RecipeApp/static/media/hearthfull.53af0b3e.svg"
+  },
+  {
+    "revision": "5bc3c5bf745f6a04d8ffd68550805a91",
+    "url": "/ReactJS-RecipeApp/static/media/warning.5bc3c5bf.svg"
+  }
+]);
